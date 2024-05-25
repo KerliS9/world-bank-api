8f94415dd44d5cd0ad32a1dd97891f37513f0958
Kerli <kerlischroeder9@gmail.com>
2024-05-24 23:14:56 -0300

    feat: get data from api

--- src/get_api.py
+++ src/get_api.py
@@ -1,8 +1,11 @@
 import requests
 
 def get_api(url):
-  data = requests.get(url)
-  print(data.status_code)
+  response = requests.get(url)
+  data = response.text
+  print(response.status_code)
+  print(data)
+  return data
 
 url = "https://api.worldbank.org/v2/country/ARG;BOL;BRA;CHL;COL;ECU;GUY;PRY;PER;SUR;URY;VEN/indicator/NY.GDP.MKTP.CD?format=json&page=1&per_page=50"
 get_api(url)
