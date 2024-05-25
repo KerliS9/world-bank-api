41cd945c5f537c024a4f5e6a6de889dbc5d1db09
Kerli <kerlischroeder9@gmail.com>
2024-05-25 18:08:15 -0300

    feat: modules get and insert data

--- src/get_api.py
+++ src/get_api.py
@@ -1,39 +1,13 @@
 import requests
-from flask import Flask, jsonify
-import psycopg2
-import os
-
-app = Flask(__name__)
 
 def get_api(url):
   response = requests.get(url)
   data = response.text
   print(response.status_code)
+  print(type(data))
   print(data)
   return data
 
 url = "https://api.worldbank.org/v2/country/ARG;BOL;BRA;CHL;COL;ECU;GUY;PRY;PER;SUR;URY;VEN/indicator/NY.GDP.MKTP.CD?format=json&page=1&per_page=50"
 get_api(url)
 
-
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.environ['POSTGRES_HOST'],
-        database=os.environ['POSTGRES_DB'],
-        user=os.environ['POSTGRES_USER'],
-        password=os.environ['POSTGRES_PASSWORD']
-    )
-    return conn
-
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT version();')
-    db_version = cur.fetchone()
-    cur.close()
-    conn.close()
-    return jsonify(db_version)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
